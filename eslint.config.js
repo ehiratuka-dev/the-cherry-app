cd7cb7afce4e180da66bdc2795f0a1ad3e4a5a7b
Eduardo Hiratuka <duduhiratuka@gmail.com>
2025-02-18 18:07:26 -0300

    Ajuste

--- eslint.config.js
+++ eslint.config.js
@@ -20,7 +20,7 @@ export default [
 			},
 		},
 		files: ['**/*.ts', '**/*.js'],
-		ignores: ['node_modules/**/*', 'dist/**/*'],
+		ignores: ['node_modules/**/*', 'dist/**/*', 'docs-out/**/*'],
 		plugins: {
 			'@typescript-eslint': typescriptEslint,
 			prettier: prettierPlugin,
